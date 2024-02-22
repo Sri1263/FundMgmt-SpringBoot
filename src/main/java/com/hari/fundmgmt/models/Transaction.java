719434f02206b4ce0cfd1e876e21d58b5bd44c00
Sri <sriharikrishnan3621@gmail.com>
2024-02-23 01:18:01 +0530

    added sequence generator for Transaction Entity

--- src/main/java/com/hari/fundmgmt/models/Transaction.java
+++ src/main/java/com/hari/fundmgmt/models/Transaction.java
@@ -5,12 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
 @Entity
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "TransIDGen", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TransIDGen")
     int transactionID;
     @Column(nullable = false)
     int senderAccNo;
